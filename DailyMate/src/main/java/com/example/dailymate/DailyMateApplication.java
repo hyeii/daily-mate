767db6df3fa8d64107ce507d6e141664537be05d
MOON <ansghddnd12@naver.com>
2024-01-21 16:30:41 +0900

    create project

---
+++ DailyMate/src/main/java/com/example/dailymate/DailyMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMateApplication.class, args);
+	}
+
+}
