5bacddf9b74af61478fba0b2ce21af562aed9711
hyeii <121pleiades@gmail.com>
2024-01-23 18:05:48 +0900

    add: 기본 설정 추가
    - recoil, react-router, scss install 및 적용
    - 컴포넌트 폴더 생성

--- daily-mate/src/App.tsx
+++ daily-mate/src/App.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./components/main/MainPage";
+import SignInPage from "./components/auth/SignInPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ daily-mate/src/components/main/MainPage.tsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div>메인화면</div>;
+};
+
+export default MainPage;
