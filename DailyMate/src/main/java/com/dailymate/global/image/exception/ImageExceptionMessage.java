c26d433c980ac3870ed9b3c83a050a13704c1f29
kimeunsol <kimeunsol95@naver.com>
2024-01-28 23:46:49 +0900

    add: S3 업로드 코드 작성

--- DailyMate/src/main/java/com/dailymate/global/image/exception/ImageExceptionMessage.java
+++ DailyMate/src/main/java/com/dailymate/global/image/exception/ImageExceptionMessage.java
@@ -7,8 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ImageExceptionMessage {
 
-    IMAGE_BAD_REQUEST("이미지가 들어오지 않았습니다."),
-    IMAGE_NOT_FOUNT("이미지 존재하지 않습니다.");
+    IMAGE_BAD_REQUEST("등록할 이미지가 존재하지 않습니다."),
+    IMAGE_URL_BAD_REQUEST("삭제할 이미지 URL이 존재하지 않습니다."),
+    IMAGE_FILE_IO_ERROR("이미지 읽기에 실패했습니다."),
+    AMAZON_S3_SERVICE_UPLOAD_ERROR("S3 서버 에러로 인해 실패했습니다."),
+    AMAZON_S3_CLIENT_UPLOAD_ERROR("S3 클라이언트 에러로 인해 실패했습니다.")
+    ;
 
     private String msg;
 }
--- DailyMate/src/main/java/com/dailymate/global/image/service/ImageServiceImpl.java
+++ DailyMate/src/main/java/com/dailymate/global/image/service/ImageServiceImpl.java
@@ -1,14 +1,22 @@
 package com.dailymate.global.image.service;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.dailymate.global.image.exception.ImageBadRequestException;
 import com.dailymate.global.image.exception.ImageExceptionMessage;
+import com.dailymate.global.image.exception.ImageNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -27,6 +35,11 @@ public class ImageServiceImpl implements ImageService{
 
     private final AmazonS3 amazonS3Client;
 
+    /**
+     * S3에 파일 업로드
+     * @param image MultipartFile
+     * @return
+     */
     @Override
     public String uploadImage(MultipartFile image) {
 
@@ -34,8 +47,40 @@ public class ImageServiceImpl implements ImageService{
             throw new ImageBadRequestException("[uploadImage]" + ImageExceptionMessage.IMAGE_BAD_REQUEST);
         }
 
+        // 업로드하는 파일 경로 만들기
+        String uploadFilePath = getFolderName();
 
-        return null;
+        // 업로드하는 파일명 만들기
+        String originalFileName = image.getOriginalFilename();
+        String uploadFileName = getUUIDName(originalFileName);
+
+        // 로컬에 파일을 저장하지 않고 업로드
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(image.getContentType());
+        objectMetadata.setContentLength(image.getSize());
+
+        // ex) 년/월/일/파일명.확장자
+        String keyName = uploadFilePath + "/" + uploadFileName;
+
+        try(InputStream inputStream = image.getInputStream()){
+
+            // 외부에 공개하는 파일인 경우 Public Read 권한을 추가, ACL 확인
+            amazonS3Client.putObject(
+                    new PutObjectRequest(bucketName, keyName, inputStream, objectMetadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        } catch (IOException e) {
+            // 파일 읽어오기 에러
+            throw new ImageBadRequestException("[uploadImage]" + ImageExceptionMessage.IMAGE_FILE_IO_ERROR);
+        } catch (AmazonS3Exception e) {
+            // AWS S3 서비스에서 반환한 예외 처리
+            throw new ImageBadRequestException("[uploadImage]" + ImageExceptionMessage.AMAZON_S3_SERVICE_UPLOAD_ERROR);
+        } catch (AmazonClientException e) {
+            // AWS S3 클라이언트에서 반환한 예외 처리
+            throw new ImageBadRequestException("[uploadImage]" + ImageExceptionMessage.AMAZON_S3_CLIENT_UPLOAD_ERROR);
+        }
+
+        return keyName;
     }
 
     @Override
@@ -45,7 +90,7 @@ public class ImageServiceImpl implements ImageService{
 
     /**
      * UUID 파일명 반환
-     * @param fileName
+     * @param fileName String
      * @return
      */
     @Override
