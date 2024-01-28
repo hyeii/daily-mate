39654567428db27467f564df0a94387112c33ca7
Yoonoi <dbddbd39@naver.com>
2024-01-29 01:22:55 +0900

    add account - exceptionMsg 추가

--- DailyMate/src/main/java/com/dailymate/domain/account/exception/AccountExceptionMessage.java
+++ DailyMate/src/main/java/com/dailymate/domain/account/exception/AccountExceptionMessage.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum AccountExceptionMessage {
 
     ACCOUNT_NOT_FOUND("가계부 존재 안함"),
-    ACCOUNT_FORBIDDEN("해당 가계부 권한 없음");
+    ACCOUNT_FORBIDDEN("해당 가계부 권한 없음"),
+    ACCOUNT_BAD_REQUEST("가계부 금액이 0원일 수 없음");
 
     private String msg;
 
