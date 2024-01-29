3c85f5bb17b52f3671b40826f19cab37dfd772f3
hyeii <121pleiades@gmail.com>
2024-01-29 23:31:44 +0900

    feat: user 로그인 api 작성
    - accesstoken + 추가 정보 응답 미반영

--- daily-mate/src/components/auth/SignInPage.tsx
+++ daily-mate/src/components/auth/SignInPage.tsx
@@ -1,8 +1,12 @@
+import axios, { AxiosResponse } from "axios";
 import { ChangeEvent, useState } from "react";
+import { useRecoilState } from "recoil";
+import { isLoginState } from "../../atoms/authAtom";
 
 const SignInPage = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
+  const [isLogged, setIsLogged] = useRecoilState(isLoginState);
 
   const handleEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setInputEmail(event.target.value);
@@ -11,7 +15,34 @@ const SignInPage = () => {
     setInputPassword(event.target.value);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (inputEmail === "") {
+      alert("이메일을 입력해주세요");
+      return;
+    }
+
+    if (inputPassword === "") {
+      alert("비밀번호를 입력해주세요");
+      return;
+    }
+
+    try {
+      const res: AxiosResponse<{ message: string }> = await axios.post(
+        "/api/user/login",
+        {
+          email: inputEmail,
+          password: inputPassword,
+        }
+      );
+      const result = res.data.message;
+
+      console.log(result);
+      // TODO : post요청 성공 시 accesstoken 받아서 내 정보 조회 재요청 => atom에 저장, isLogin true처리
+    } catch (error) {
+      console.error("로그인 오류:", error);
+      alert("입력 정보를 확인해주세요");
+    }
+  };
   return (
     <div>
       <h3>로그인 페이지</h3>
