d7ca07e585c715fd4fe94ac0b13229b486a889e6
kimeunsol <kimeunsol95@naver.com>
2024-02-01 21:08:13 +0900

    add: Diary Repository 추가

---
+++ DailyMate/src/main/java/com/dailymate/domain/diary/dao/DiaryRepository.java
@@ -0,0 +1,9 @@
+package com.dailymate.domain.diary.dao;
+
+import com.dailymate.domain.diary.domain.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiaryRepository extends JpaRepository<Diary, Long> {
+}
