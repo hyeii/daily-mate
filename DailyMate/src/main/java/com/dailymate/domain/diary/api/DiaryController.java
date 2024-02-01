c874dd76535e2207f8cf10ab88809f524e649a5a
kimeunsol <kimeunsol95@naver.com>
2024-02-01 21:55:33 +0900

    add: Diary controller 추가

--- DailyMate/src/main/java/com/dailymate/domain/diary/api/DiaryController.java
+++ DailyMate/src/main/java/com/dailymate/domain/diary/api/DiaryController.java
@@ -1,2 +1,16 @@
-package com.dailymate.domain.diary.api;public class DiaryController {
+package com.dailymate.domain.diary.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Diary", description = "일기 API Document")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/diary")
+@Slf4j
+public class DiaryController {
+
 }
