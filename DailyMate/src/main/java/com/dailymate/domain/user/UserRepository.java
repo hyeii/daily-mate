16a1870dbb0819f8ad1d4d2c414b9fbbd0831d0e
kimeunsol <kimeunsol95@naver.com>
2024-02-08 17:35:07 +0900

    add: Diary UserRepository 파일 임시 추가

--- DailyMate/src/main/java/com/dailymate/domain/user/UserRepository.java
+++ DailyMate/src/main/java/com/dailymate/domain/user/UserRepository.java
@@ -1,2 +1,8 @@
-package com.dailymate.domain.user;public class UserRepository {
+package com.dailymate.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
