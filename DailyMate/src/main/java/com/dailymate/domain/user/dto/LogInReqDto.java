6e03fbeaeafe7b69eed41a7d0ac24b79d5971a26
Yoonoi <dbddbd39@naver.com>
2024-02-12 18:43:54 +0900

    add user - 로그인용 dto 생성

---
+++ DailyMate/src/main/java/com/dailymate/domain/user/dto/LogInReqDto.java
@@ -0,0 +1,12 @@
+package com.dailymate.domain.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LogInReqDto {
+
+    private String email;
+    private String password;
+
+
+}
---
+++ DailyMate/src/main/java/com/dailymate/domain/user/dto/LogInResDto.java
@@ -0,0 +1,14 @@
+package com.dailymate.domain.user.dto;
+
+public class LogInResDto {
+
+    private String accessToken;
+    private String refreshToken;
+    private String email;
+    private String nickName;
+
+    private String image;
+    private String profile;
+    private String type;
+
+}
