8ea45ab99eb3e0a099541add924f9a85fde19051
Yoonoi <dbddbd39@naver.com>
2024-02-16 22:14:07 +0900

    refactor userDetails - 사용자 권한 타입 수정

--- DailyMate/src/main/java/com/dailymate/global/common/security/UserDetailsImpl.java
+++ DailyMate/src/main/java/com/dailymate/global/common/security/UserDetailsImpl.java
@@ -1,48 +1,60 @@
 package com.dailymate.global.common.security;
 
 import com.dailymate.domain.user.constant.UserType;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.FetchType;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Spring Security에서 사용자의 정보를 담는 인터페이스를 구현한 클래스
  */
-@Getter
 @Builder
 @AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
-//    private Long userId;
     private String email; // username
+    private String authority;
 
-    private String password;
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @Builder.Default
+//    private List<String> roles = new ArrayList<>(); // 유저가 가지고 있는 권한 목록
+//
+//    /**
+//     * 계정의 권한 목록을 리턴
+//     */
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return this.roles.stream()
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+//    }
 
-//    private String authority;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Builder.Default
-    private List<String> roles = new ArrayList<>(); // 유저가 가지고 있는 권한 목록
-
-    /**
-     * 계정의 권한 목록을 리턴
-     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
+        List<GrantedAuthority> auth = new ArrayList<>();
+
+        if(authority.equals(UserType.ROLE_ADMIN.getRole()))
+            authority = "ROLE_ADMIN";
+        else if(authority.equals(UserType.ROLE_SOCIAL.getRole()))
+            authority = "ROLE_SOCIAL";
+        else
+            authority = "ROLE_USER";
+
+        auth.add(new SimpleGrantedAuthority(authority));
+
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
     }
 
     /**
