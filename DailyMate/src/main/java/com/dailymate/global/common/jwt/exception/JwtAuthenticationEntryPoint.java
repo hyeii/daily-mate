fbb0ca9cb93920eff7d6b18ce16742d1b40c1e6d
Yoonoi <dbddbd39@naver.com>
2024-02-16 16:12:04 +0900

    add exceptionHandler - 필터용 exception(401, 403) 추가

---
+++ DailyMate/src/main/java/com/dailymate/global/common/jwt/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.dailymate.global.common.jwt.exception;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 사용자 정보가 잘못되었거나,
+ * 토큰이 유효하지 않은 경우에 대비하는 클래스(401 Unauthorized)
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
