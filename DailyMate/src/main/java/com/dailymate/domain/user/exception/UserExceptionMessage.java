4bdbfc1f7e1c6fd473a54a7d169b4af381faf8e4
Yoonoi <dbddbd39@naver.com>
2024-02-18 02:36:55 +0900

    add user - exceptionMessage 추가

--- DailyMate/src/main/java/com/dailymate/domain/user/exception/UserExceptionMessage.java
+++ DailyMate/src/main/java/com/dailymate/domain/user/exception/UserExceptionMessage.java
@@ -11,6 +11,8 @@ public enum UserExceptionMessage {
     USER_FORBIDDEN("권한이 없는 사용자입니다."),
     SIGN_UP_BAD_REQUEST("회원가입에 필요한 정보가 불충분합니다."),
     PASSWORD_NOT_MATCH_REGEX("비밀번호는 8~16자 이내의 영문, 숫자, 특수문자를 포함해야 합니다."),
+    PASSWORD_INCORRECT("비밀번호가 틀립니다."),
+    PASSWORD_MUST_BE_DIFFERENT("현재 비밀번호와 다른 비밀번호를 입력해야 합니다."),
     NICKNAME_DUPLICATED("이미 사용중인 닉네임입니다.");
 
     private String msg;
