e835b76cf262a9de201351886db273f31fd4229b
Yoonoi <dbddbd39@naver.com>
2024-02-19 02:12:51 +0900

    refactor friend - jpql에 적용되는 생성자 로직 수정

--- DailyMate/src/main/java/com/dailymate/domain/friend/dto/FriendRequestDto.java
+++ DailyMate/src/main/java/com/dailymate/domain/friend/dto/FriendRequestDto.java
@@ -1,10 +1,8 @@
 package com.dailymate.domain.friend.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class FriendRequestDto {
 
     private Long fromId;
@@ -14,5 +12,15 @@ public class FriendRequestDto {
     private String nickname;
     private String image;
     private String  profile;
+
+    // jdcl인가 거기서 lombok생성자는 인식을 잘못해서 직접 구현!
+    public FriendRequestDto(Long fromId, String requestDate, String email, String nickname, String image, String profile) {
+        this.fromId = fromId;
+        this.requestDate = requestDate;
+        this.email = email;
+        this.nickname = nickname;
+        this.image = image;
+        this.profile = profile;
+    }
 
 }
