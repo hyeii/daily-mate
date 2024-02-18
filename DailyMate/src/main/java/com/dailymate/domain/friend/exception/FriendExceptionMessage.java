0a77ed6c25592d4b3126e6ae3e23eb73e63194a4
Yoonoi <dbddbd39@naver.com>
2024-02-19 02:11:57 +0900

    add friend - exception msg 추가

--- DailyMate/src/main/java/com/dailymate/domain/friend/exception/FriendExceptionMessage.java
+++ DailyMate/src/main/java/com/dailymate/domain/friend/exception/FriendExceptionMessage.java
@@ -11,7 +11,8 @@ public enum FriendExceptionMessage {
     CANNOT_REQUEST_YOURSELF("본인에게 친구 신청을 할 수 없습니다."),
     FRIEND_NOT_FOUND("해당 친구 데이터를 찾을 수 없습니다."),
     ALREADY_FRIEND("이미 친구거나 대기중입니다."),
-    YOU_ARE_NOT_FRIEND("친구 상태가 아닙니다.");
+    YOU_ARE_NOT_FRIEND("친구 상태가 아닙니다."),
+    NOT_WAITING("친구 요청 대기중이 아닙니다.");
 
     private String msg;
 
