256ac25e0377ab35e24afedbe7cd042fd2554d98
Yoonoi <dbddbd39@naver.com>
2024-02-19 03:02:04 +0900

    feat friend - 친구 신청 승낙/거부, 친구 끊기, 친구 전체조회, 친구 상세조회 구현

--- DailyMate/src/main/java/com/dailymate/domain/friend/api/FriendController.java
+++ DailyMate/src/main/java/com/dailymate/domain/friend/api/FriendController.java
@@ -1,6 +1,6 @@
 package com.dailymate.domain.friend.api;
 
-import com.dailymate.domain.friend.dto.FriendRequestDto;
+import com.dailymate.domain.friend.dto.FriendInfoDto;
 import com.dailymate.domain.friend.service.FriendService;
 import com.dailymate.global.dto.MessageDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -35,7 +35,7 @@ public class FriendController {
             description = "로그인 사용자에게 친구 신청을 걸어 대기중인 회원들의 정보를 리스트로 조회합니다."
     )
     @GetMapping("/request/all")
-    public ResponseEntity<List<FriendRequestDto>> findFriendRequestList(@RequestHeader(ACCESS_TOKEN) String token) {
+    public ResponseEntity<List<FriendInfoDto>> findFriendRequestList(@RequestHeader(ACCESS_TOKEN) String token) {
         return ResponseEntity.ok(friendService.findFriendRequestList(token));
     }
 
@@ -44,8 +44,56 @@ public class FriendController {
             description = "로그인 사용자에게 친구 신청을 걸어 대기중인 회원의 정보를 조회합니다."
     )
     @GetMapping("/request/{friendId}")
-    public ResponseEntity<FriendRequestDto> findFriendRequest(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
+    public ResponseEntity<FriendInfoDto> findFriendRequest(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
         return ResponseEntity.ok(friendService.findFriendRequest(token, friendId));
+    }
+
+    @Operation(
+            summary = "친구 신청 승낙",
+            description = "친구 신청을 승낙합니다."
+    )
+    @PatchMapping("/request/{friendId}")
+    public ResponseEntity<MessageDto> acceptFriend(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
+        friendService.acceptFriend(token, friendId);
+        return ResponseEntity.ok(MessageDto.message("ACCEPT SUCCESS"));
+    }
+
+    @Operation(
+            summary = "친구 신청 거부",
+            description = "친구 신청을 거부합니다."
+    )
+    @DeleteMapping("/request/{friendId}")
+    public ResponseEntity<MessageDto> rejectFriend(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
+        friendService.rejectFriend(token, friendId);
+        return ResponseEntity.ok(MessageDto.message("REJECT SUCCESS"));
+    }
+
+    @Operation(
+            summary = "친구 끊기",
+            description = "친구 상태를 끊습니다."
+    )
+    @DeleteMapping("/{friendId}")
+    public ResponseEntity<MessageDto> deleteFriend(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
+        friendService.deleteFriend(token, friendId);
+        return ResponseEntity.ok(MessageDto.message("DELETE SUCCESS"));
+    }
+
+    @Operation(
+            summary = "친구 리스트 전체 조회",
+            description = "친구 상태인 회원들의 정보를 조회합니다."
+    )
+    @GetMapping("/all")
+    public ResponseEntity<List<FriendInfoDto>> findFriendList(@RequestHeader(ACCESS_TOKEN) String token) {
+        return ResponseEntity.ok(friendService.findFriendList(token));
+    }
+
+    @Operation(
+            summary = "친구 상세 조회",
+            description = "친구 상태인 회원의 정보를 조회합니다."
+    )
+    @GetMapping("/{friendId}")
+    public ResponseEntity<FriendInfoDto> findFriend(@RequestHeader(ACCESS_TOKEN) String token, @PathVariable Long friendId) {
+        return ResponseEntity.ok(friendService.findFriend(token, friendId));
     }
 
 }
--- DailyMate/src/main/java/com/dailymate/domain/friend/service/FriendService.java
+++ DailyMate/src/main/java/com/dailymate/domain/friend/service/FriendService.java
@@ -1,7 +1,6 @@
 package com.dailymate.domain.friend.service;
 
 import com.dailymate.domain.friend.dto.FriendInfoDto;
-import com.dailymate.domain.friend.dto.FriendRequestDto;
 
 import java.util.List;
 
@@ -9,9 +8,8 @@ public interface FriendService {
 
     void addFriendRequest(String token, Long toId); // 친구가 되고싶은 사람의 ID
 
-    List<FriendRequestDto> findFriendRequestList(String token);
-
-    FriendRequestDto findFriendRequest(String token, Long friendId); // friendId가 맞나
+    List<FriendInfoDto> findFriendRequestList(String token);
+    FriendInfoDto findFriendRequest(String token, Long friendId); // friendId가 맞나
 
     void acceptFriend(String token, Long friendId);
     void rejectFriend(String token, Long friendId);
