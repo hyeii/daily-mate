888b9fef703ff918bdb1f5afd3c55198914f0863
Yoonoi <dbddbd39@naver.com>
2024-02-19 01:00:46 +0900

    feat user - user와 friend를 join하여 각 정보를 추출하는 query 추가

--- DailyMate/src/main/java/com/dailymate/domain/user/dao/UserRepository.java
+++ DailyMate/src/main/java/com/dailymate/domain/user/dao/UserRepository.java
@@ -2,7 +2,10 @@ package com.dailymate.domain.user.dao;
 
 import com.dailymate.domain.user.constant.UserType;
 import com.dailymate.domain.user.domain.Users;
+import com.dailymate.domain.user.dto.response.UserSearchInfoDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +25,11 @@ public interface UserRepository extends JpaRepository<Users, Long> {
 
     // 친구 신청 메서드에서 탈퇴한 회원인지 체크하기 위함
     Boolean existsByUserId(Long userId);
+
+    @Query(value = "SELECT new com.dailymate.domain.user.dto.response.UserSearchInfoDto(u.userId, u.email, u.nickname, u.image, u.profile, f.status, f.requestDate)" +
+            "FROM Users u " +
+            "JOIN Friend f ON (u.userId = f.fromId AND f.toId = :userId) OR (u.userId = f.toId AND f.fromId = :userId) " +
+            "WHERE u.userId = :friendId")
+    Optional<UserSearchInfoDto> checkFriendStatus(@Param("userId") Long userId, @Param("friendId") Long friendId);
 
 }
