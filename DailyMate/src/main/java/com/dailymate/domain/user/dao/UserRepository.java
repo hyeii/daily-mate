1f5ebce899b75b301fcd7d4e91542812f043f97d
Yoonoi <dbddbd39@naver.com>
2024-02-18 19:53:00 +0900

    feat user - 회원 전체조회, 상세조회, 검색 기능 구현

--- DailyMate/src/main/java/com/dailymate/domain/user/dao/UserRepository.java
+++ DailyMate/src/main/java/com/dailymate/domain/user/dao/UserRepository.java
@@ -1,13 +1,21 @@
 package com.dailymate.domain.user.dao;
 
 import com.dailymate.domain.user.domain.Users;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmailAndDeletedAtIsNull(String email);
+
+    List<Users> findByDeletedAtIsNull();
+    Optional<Users> findByUserIdAndDeletedAtIsNull(Long userId); // 관리자체크는 서비스에서 할거니까
+
+    List<Users> findByNicknameContainingAndDeletedAtIsNull(String nickname);
+
     
     Boolean existsByEmailAndDeletedAtIsNull(String email); // 이메일 중복검사
     Boolean existsByNicknameAndDeletedAtIsNull(String nickname); // 닉네임 중복검사
