25fbae7dd5d7b29d5a0be66c55c3677907c20ed3
Yoonoi <dbddbd39@naver.com>
2024-02-21 02:06:54 +0900

    add oauth - handler 생성

---
+++ DailyMate/src/main/java/com/dailymate/global/common/oauth/OAuth2SuccessHandler.java
@@ -0,0 +1,50 @@
+package com.dailymate.global.common.oauth;
+
+import com.dailymate.domain.user.dao.RefreshTokenRedisRepository;
+import com.dailymate.domain.user.domain.CustomOAuth2User;
+import com.dailymate.domain.user.domain.RefreshToken;
+import com.dailymate.global.common.jwt.JwtTokenDto;
+import com.dailymate.global.common.jwt.JwtTokenProvider;
+import com.dailymate.global.common.jwt.constant.JwtTokenExpiration;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        try {
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+            JwtTokenDto tokenDto = jwtTokenProvider.generateToken(authentication);
+
+            RefreshToken refreshToken = RefreshToken.builder()
+                    .email(oAuth2User.getEmail())
+                    .refreshToken(tokenDto.getRefreshToken())
+                    .expiration(JwtTokenExpiration.REFRESH_TOKEN_EXPIRATION_TIME.getTime())
+                    .build();
+
+            refreshTokenRedisRepository.save(refreshToken);
+        } catch (AuthenticationException e) {
+            log.error("[OAuth2SuccessHandler] 인가 에러 =======================");
+            request.setAttribute("oauth2Exception", e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
