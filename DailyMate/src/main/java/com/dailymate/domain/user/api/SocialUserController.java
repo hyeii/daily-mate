c2394e7eecbbc727380a2f5f9c2a2dc634123f13
Yoonoi <dbddbd39@naver.com>
2024-02-21 15:07:30 +0900

    add user - 소셜회원용 메서드추가

---
+++ DailyMate/src/main/java/com/dailymate/domain/user/api/SocialUserController.java
@@ -0,0 +1,19 @@
+package com.dailymate.domain.user.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class SocialUserController {
+
+//    @PostMapping("/oauth2/authorization/google")
+//    public ResponseEntity<?> googleLogin() {
+//        return ResponseEntity.ok("굿");
+//    }
+
+}
