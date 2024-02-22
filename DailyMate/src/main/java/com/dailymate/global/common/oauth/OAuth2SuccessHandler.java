c5cfcf8e559a7e10091dcc80343bb6e2fef7d07b
Yoonoi <dbddbd39@naver.com>
2024-02-23 00:20:58 +0900

    fix oauth2SuccessHandler - 로그인 성공 후 response 수정

--- DailyMate/src/main/java/com/dailymate/global/common/oauth/OAuth2SuccessHandler.java
+++ DailyMate/src/main/java/com/dailymate/global/common/oauth/OAuth2SuccessHandler.java
@@ -1,11 +1,13 @@
 package com.dailymate.global.common.oauth;
 
 import com.dailymate.domain.user.dao.RefreshTokenRedisRepository;
-import com.dailymate.domain.user.domain.CustomOAuth2User;
 import com.dailymate.domain.user.domain.RefreshToken;
+import com.dailymate.domain.user.dto.response.LogInResDto;
 import com.dailymate.global.common.jwt.JwtTokenDto;
 import com.dailymate.global.common.jwt.JwtTokenProvider;
 import com.dailymate.global.common.jwt.constant.JwtTokenExpiration;
+import com.dailymate.global.common.security.UserDetailsImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -29,21 +31,45 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         try {
-            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+            UserDetailsImpl oAuth2User = (UserDetailsImpl)authentication.getPrincipal();
 
-            log.info("email : {}", oAuth2User.getEmail());
-            log.info("oAuth2User : {}", oAuth2User);
-            JwtTokenDto tokenDto = jwtTokenProvider.generateTokenOfSocial(authentication);
+            JwtTokenDto tokenDto = jwtTokenProvider.generateToken(authentication);
 
             RefreshToken refreshToken = RefreshToken.builder()
-                    .email(oAuth2User.getEmail())
+                    .email(oAuth2User.getUsername())
                     .refreshToken(tokenDto.getRefreshToken())
                     .expiration(JwtTokenExpiration.REFRESH_TOKEN_EXPIRATION_TIME.getTime())
                     .build();
 
-            refreshTokenRedisRepository.save(refreshToken);
+            log.info("[SUCCESS_HANDLER] accessToken : {}", tokenDto.getAccessToken());
+            refreshTokenRedisRepository.save(refreshToken); // 소셜로그인
+
+            // 리디렉션으로 Param에 넣어서 프론트에 값을 보내면 값이 노출되어서 보안에 취약하니까
+            // JSON형식으로 응답을 보내자
+
+            // LoginResDto를 보낼거니까 우선 만들기
+            LogInResDto logInResDto = LogInResDto.builder()
+                    .accessToken(tokenDto.getAccessToken())
+                    .refreshToken(tokenDto.getRefreshToken())
+                    .email(oAuth2User.getUsername())
+                    .nickName(oAuth2User.getName())
+                    .image(oAuth2User.getUser().getImage())
+                    .profile(oAuth2User.getUser().getProfile())
+                    .type(oAuth2User.getAuthorities().toString())
+                    .build();
+
+            // JSON 형식으로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+            String userInfo = objectMapper.writeValueAsString(logInResDto);
+
+            // 응답 생성
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(userInfo);
+
+            log.info("[SUCCESS_HANDLER] 응답 생성 완료.");
         } catch (AuthenticationException e) {
-            log.error("[OAuth2SuccessHandler] 인가 에러 =======================");
+            log.error("[SUCCESS_HANDLER] 인가 에러 =======================");
             request.setAttribute("oauth2Exception", e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
