ff13b649cc403c92615ecd8b163559fa66e363a3
MOON <ansghddnd12@naver.com>
2024-02-26 00:56:01 +0900

    init : todoPage init

---
+++ daily-mate/src/components/todo/DailyTodoList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DailyTodoList: React.FC = () => {
+  return (
+    <div>
+      {/* 일별 Todo 리스트를 렌더링하는 로직 */}
+      <h2>일별 Todo 리스트</h2>
+      {/* Todo 리스트 아이템들을 여기에 렌더링 */}
+    </div>
+  );
+};
+
+export default DailyTodoList;
--- daily-mate/src/components/todo/TodoPage.tsx
+++ daily-mate/src/components/todo/TodoPage.tsx
@@ -1,5 +1,15 @@
+import React, { useState } from "react";
+import DailyTodoList from "./DailyTodoList";
+
 const TodoPage = () => {
-  return <div>할 일 </div>;
+  const [view, setView] = useState<"daily" | "monthly">("daily");
+  return (
+    <div>
+      <button onClick={() => setView("daily")}>일</button>
+      <button onClick={() => setView("monthly")}>월</button>
+      {view === "daily" ? <DailyTodoList /> : <div>월별조회</div>}
+    </div>
+  );
 };
 
 export default TodoPage;
