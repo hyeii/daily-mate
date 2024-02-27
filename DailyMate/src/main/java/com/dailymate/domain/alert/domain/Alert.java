b7f2767370f2819262f992bb2490ab4e08df75ad
Yoonoi <dbddbd39@naver.com>
2024-02-27 18:38:46 +0900

    refactor alert - basetime extends 삭제

--- DailyMate/src/main/java/com/dailymate/domain/alert/domain/Alert.java
+++ DailyMate/src/main/java/com/dailymate/domain/alert/domain/Alert.java
@@ -1,12 +1,13 @@
 package com.dailymate.domain.alert.domain;
 
 import com.dailymate.domain.alert.constant.AlertType;
-import com.dailymate.global.common.BaseTime;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,7 +16,7 @@ import javax.persistence.*;
 @Entity
 @DynamicInsert
 @DynamicUpdate
-public class Alert extends BaseTime {
+public class Alert {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,11 +26,20 @@ public class Alert extends BaseTime {
 
     private Long fromId;
 
+    private Long diaryId;
+
     private String content;
 
     @Enumerated(EnumType.STRING)
     private AlertType type;
 
     private String url;
+
+    private String createdAt;
+
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
 
 }
