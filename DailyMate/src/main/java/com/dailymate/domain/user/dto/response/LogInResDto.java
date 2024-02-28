eb706235932d74a5a7498c32b930b6db85c1f52d
Yoonoi <dbddbd39@naver.com>
2024-02-29 01:35:20 +0900

    add oauth google - redirect를 위한 생성자 추가

--- DailyMate/src/main/java/com/dailymate/domain/user/dto/response/LogInResDto.java
+++ DailyMate/src/main/java/com/dailymate/domain/user/dto/response/LogInResDto.java
@@ -1,10 +1,14 @@
 package com.dailymate.domain.user.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LogInResDto {
 
     private String accessToken;
